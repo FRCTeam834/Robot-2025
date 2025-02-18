9e1d760ce6b5e12200a0428bd736344fcf8d5f2e
BuildTools <unconfigured@null.spigotmc.org>
2025-02-18 17:00:50 -0500

    ready for sysid

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -7,19 +7,21 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.revrobotics.AbsoluteEncoder;
-import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.AbsoluteEncoderConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.utility.TunableNumber;
-////import com.revrobotics.spark.config.AlternateEncoderConfig.Type;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class Arm extends SubsystemBase {
@@ -27,91 +29,85 @@ public class Arm extends SubsystemBase {
 
   //Declare motors
   private final SparkMax intakeMotor;
-  private final SparkMax armMotor;
+  private final SparkMax pivotMotor;
   
   //Arm absolute encoder
-  private final AbsoluteEncoder armAbsoluteEncoder;
+  private final AbsoluteEncoder pivotAbsoluteEncoder;
 
   //Arm PID constants
-  private static final TunableNumber armkP = new TunableNumber("Arm/armkP");
-  private static final TunableNumber armkI = new TunableNumber("Arm/armkI");
-  private static final TunableNumber armkD = new TunableNumber("Arm/armkD");
+  private static final TunableNumber pivotkP = new TunableNumber("Arm/armkP");
+  private static final TunableNumber pivotkI = new TunableNumber("Arm/armkI");
+  private static final TunableNumber pivotkD = new TunableNumber("Arm/armkD");
   
   //Arm feedforward constants
-  private static final TunableNumber armkS = new TunableNumber("Arm/armkS"); //static friction
-  private static final TunableNumber armkG = new TunableNumber("Arm/armkG"); //gravity
-  private static final TunableNumber armkV = new TunableNumber("Arm/armkV"); //velocity
+  private static final TunableNumber pivotkS = new TunableNumber("Arm/armkS"); //static friction
+  private static final TunableNumber pivotkG = new TunableNumber("Arm/armkG"); //gravity
+  private static final TunableNumber pivotkV = new TunableNumber("Arm/armkV"); //velocity
 
   //Initialize arm feedforward
   private ArmFeedforward armFeedforward = new ArmFeedforward(0.0, 0.0, 0.0);
   
   //Arm PID controller
-  private SparkClosedLoopController armPIDController;
+  private ProfiledPIDController trapezoidPID = new ProfiledPIDController(0, 0, 0, 
+    new TrapezoidProfile.Constraints(Units.degreesToRadians(180), Units.degreesToRadians(180))
+  );
 
   //Booleans for if the arm is stopped
-  private boolean armStopped = false;
-  private boolean intakeStopped = false;
+  private boolean armStopped = true;
   
   //Arm angle setpoint
-  private double angleSetpoint = 0.0; //TODO: Get units
+  private double pivotAngleSetpoint = 0.0; 
 
-  //Intake voltage
-  private double intakeVoltage = 0.0; //TODO: Find correct values for intake voltages
-  
+  //Intake voltag  
   //Configuration for the motors
-  private SparkMaxConfig armMotorConfig = new SparkMaxConfig();
+  private SparkMaxConfig pivotMotorConfig = new SparkMaxConfig();
   private SparkMaxConfig intakeMotorConfig = new SparkMaxConfig();
+  private AbsoluteEncoderConfig pivotEncoderConfig = new AbsoluteEncoderConfig();
  
   //Arm PID and feedforward constants
   static {
     //TODO: Initialize default constants
-    armkP.initDefault(1);
-    armkI.initDefault(0);
-    armkD.initDefault(0);
+    pivotkP.initDefault(1);
+    pivotkI.initDefault(0);
+    pivotkD.initDefault(0);
 
-    armkS.initDefault(0);
-    armkG.initDefault(0);
-    armkV.initDefault(0);
+    pivotkS.initDefault(0);
+    pivotkG.initDefault(0);
+    pivotkV.initDefault(0);
   }
 
   //Arm constructor
   public Arm() {
     //Initialize motor
-    armMotor = new SparkMax(17, MotorType.kBrushless);
+    pivotMotor = new SparkMax(17, MotorType.kBrushless);
     intakeMotor = new SparkMax(18, MotorType.kBrushless);
     
     //Initialize arm absolute encoder
-    armAbsoluteEncoder = armMotor.getAbsoluteEncoder();
-
-    //Initialize arm PID controller
-    armPIDController = armMotor.getClosedLoopController();
+    pivotAbsoluteEncoder = pivotMotor.getAbsoluteEncoder();
     
     //Configure motors and encoders
-    //TODO: Arm motor configurations
-    armMotorConfig
+    pivotMotorConfig
     .idleMode(IdleMode.kBrake)
+    .smartCurrentLimit(40)
+    .voltageCompensation(12)
     .inverted(false);
 
-    //TODO: Arm Encoder configurations
-    armMotorConfig.encoder
-    .positionConversionFactor(0)
-    .velocityConversionFactor(0);
-
-    //TODO: Arm PID configurations
-    armMotorConfig.closedLoop
-    .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
-    .pid(armkP.get(), armkI.get(), armkD.get());
-
-    //TODO: Intake motor configurations
     intakeMotorConfig
     .idleMode(IdleMode.kBrake)
+    .smartCurrentLimit(40)
+    .voltageCompensation(12)
     .inverted(false);
 
+    pivotEncoderConfig
+    .positionConversionFactor(2 * Math.PI)
+    .velocityConversionFactor(2 * Math.PI / 60)
+    .zeroOffset(0)
+    .setSparkMaxDataPortConfig(); //revolutions
+
     //Apply configurations
-    armMotor.configure(armMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    pivotMotor.configure(pivotMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     intakeMotor.configure(intakeMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-    //Send all data to the Smart Dashboard
     SmartDashboard.putData(this);
   }
 
@@ -120,48 +116,46 @@ public class Arm extends SubsystemBase {
     // This method will be called once per scheduler run
     
     //Update the arm PID if the constants have changed
-    if (armkP.hasChanged(hashCode()) || armkI.hasChanged(hashCode()) || armkD.hasChanged(hashCode())) {
-      armMotorConfig.closedLoop.pid(armkP.get(), armkI.get(), armkD.get());
-      armMotor.configureAsync(armMotorConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+    if (pivotkP.hasChanged(hashCode()) || pivotkI.hasChanged(hashCode()) || pivotkD.hasChanged(hashCode())) {
+      trapezoidPID.setPID(pivotkP.get(), pivotkI.get(), pivotkD.get());
     }
 
     //Update the arm feedforward if the constants have changed
-    if (armkS.hasChanged(hashCode()) || armkG.hasChanged(hashCode()) || armkV.hasChanged(hashCode())) {
-      armFeedforward = new ArmFeedforward(armkS.get(), armkG.get(), armkV.get());
+    if (pivotkS.hasChanged(hashCode()) || pivotkG.hasChanged(hashCode()) || pivotkV.hasChanged(hashCode())) {
+      armFeedforward = new ArmFeedforward(pivotkS.get(), pivotkG.get(), pivotkV.get());
     }
 
     //Set the position of the arm motor
     if (!armStopped) {
-      armPIDController.setReference(angleSetpoint, ControlType.kPosition, null, armFeedforward.calculate(armAbsoluteEncoder.getPosition(), armAbsoluteEncoder.getVelocity()));
-    }
-
-    //Set the voltage of the intake motor
-    if (!intakeStopped) {
-      intakeMotor.setVoltage(intakeVoltage);
+      pivotMotor.setVoltage(trapezoidPID.calculate(pivotAngleSetpoint) + 
+      armFeedforward.calculate(trapezoidPID.getSetpoint().position, trapezoidPID.getSetpoint().velocity));
     }
   }
 
   //Stop all motors
   public void stop () {
     armStopped = true;
-    intakeStopped = true;
-    armMotor.setVoltage(0.0);
+    pivotMotor.setVoltage(0.0);
     intakeMotor.setVoltage(0.0);
   }
 
   //Update the arm angle setpoint
-  public void setArmAngleSetpoint (double angle) {
+  public void setDesiredPivotAngle (double angle) {
     armStopped = false;
-    angleSetpoint = angle;
+    pivotAngleSetpoint = MathUtil.clamp(angle, 0, Math.PI);
   }
 
-  //Update the desired intake voltage
   public void setIntakeVoltage (double volts) {
-    intakeStopped = false;
-    intakeVoltage = volts;
+    intakeMotor.setVoltage(volts);
+  }
+
+  public double getCurrentPivotAngle() {
+    return pivotAbsoluteEncoder.getPosition();
   }
 
   public void initSendable (SendableBuilder builder) {
     builder.setSmartDashboardType("Arm");
+    builder.addDoubleProperty("CurrentAngle", this::getCurrentPivotAngle, null);
+    builder.addDoubleProperty("SetpointAngle", () -> {return this.pivotAngleSetpoint;}, null);
   }
 }
