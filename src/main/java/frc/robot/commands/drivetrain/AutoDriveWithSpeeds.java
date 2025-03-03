3fc249081ce10963616098bd3e8e4f6fd39f64bf
BuildTools <unconfigured@null.spigotmc.org>
2025-03-02 20:46:58 -0500

    reworked autodrive

--- src/main/java/frc/robot/commands/drivetrain/AutoDriveWithSpeeds.java
+++ src/main/java/frc/robot/commands/drivetrain/AutoDriveWithSpeeds.java
@@ -4,10 +4,6 @@
 
 package frc.robot.commands.drivetrain;
 
-import java.util.Arrays;
-
-import com.fasterxml.jackson.databind.node.POJONode;
-
 import edu.wpi.first.math.controller.HolonomicDriveController;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
@@ -29,7 +25,6 @@ import frc.robot.Constants.SwerveConstants;
 import frc.robot.Constants;
 import frc.robot.OI;
 import frc.robot.subsystems.drivetrain.DriveTrain;
-import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.utility.PoseEstimator;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
@@ -44,11 +39,7 @@ public class AutoDriveWithSpeeds extends Command {
      new TrapezoidProfile.Constraints(Units.degreesToRadians(180), Units.degreesToRadians(180)))
   );
 
-  private double desiredLinearVelocity;
   private Pose2d closestScoringPose;
-
-  private double joystickTolerance = 0.15;
-  private boolean doesCalculateScoringPosition = true;
 
   private Trajectory desiredTrajectory;
   private TrajectoryConfig trajectoryConfig;
@@ -66,69 +57,52 @@ public class AutoDriveWithSpeeds extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    
+    driveTimer.stop();
+    driveTimer.reset();
+
+    Pose2d robotPose = poseEstimator.getPoseEstimate();
+
+    double bestCost = Double.MAX_VALUE;
+    for(Pose2d scoringPose : Constants.scoringPoses) {
+      double distanceError = robotPose.getTranslation().getDistance(scoringPose.getTranslation());
+      double angleError = Math.abs(robotPose.getRotation().minus(scoringPose.getRotation()).getRadians());
+
+      double cost = (distanceError * 0.5 + angleError * 5);
+      if (cost < bestCost) {
+        bestCost = cost;
+        closestScoringPose = scoringPose; 
+      }
+    }
+
+    trajectoryConfig = new TrajectoryConfig(2, 2);
+    trajectoryConfig.setStartVelocity(driveTrain.getRobotVeloMagnitude());  
+    trajectoryConfig.setKinematics(driveTrain.getKinematics());
+
+    desiredTrajectory = TrajectoryGenerator.generateTrajectory(
+      robotPose, 
+      null, 
+      closestScoringPose, 
+      trajectoryConfig);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double rightJoystickx = OI.getRightJoystickX();
-    double rightJoysticky = OI.getRightJoystickY();
-    double leftJoystickx = OI.getLeftJoystickX();
+    driveTimer.start();
+    ChassisSpeeds speeds = holonomicDriveController.calculate(
+      poseEstimator.getPoseEstimate(), 
+      desiredTrajectory.sample(driveTimer.get()),
+      closestScoringPose.getRotation()
+    );
 
-    Pose2d robotPose = poseEstimator.getPoseEstimate();
-
-    // While matt is manually driving robot recalculate most optimal scoring position
-    if (doesCalculateScoringPosition) {
-      driveTimer.stop();
-
-      double bestCost = Double.MAX_VALUE;
-      for(Pose2d scoringPose : Constants.scoringPoses) {
-        double distanceError = robotPose.getTranslation().getDistance(scoringPose.getTranslation());
-        double angleError = Math.abs(robotPose.getRotation().minus(scoringPose.getRotation()).getRadians());
-
-        double cost = (distanceError * 0.5 + angleError * 5);
-        if (cost < bestCost) {
-          bestCost = cost;
-          closestScoringPose = scoringPose; 
-        }
-      }
-
-      trajectoryConfig = new TrajectoryConfig(2, 2);
-      trajectoryConfig.setStartVelocity(driveTrain.getRobotVeloMagnitude());      
-    }
-
-    if(Math.abs(rightJoystickx) > joystickTolerance || Math.abs(rightJoysticky) > joystickTolerance || Math.abs(leftJoystickx) > joystickTolerance) {
-      driveTrain.drive(
-        rightJoystickx * SwerveConstants.MAX_TRANSLATION_SPEED, 
-        -rightJoysticky * SwerveConstants.MAX_TRANSLATION_SPEED, 
-        -leftJoystickx * SwerveConstants.MAX_STEER_SPEED
-      );
-      doesCalculateScoringPosition = true;
-    } else {
-      driveTimer.start();
-
-      desiredTrajectory = TrajectoryGenerator.generateTrajectory(
-        robotPose, 
-        null, 
-        closestScoringPose, 
-        trajectoryConfig);
-
-      ChassisSpeeds speeds = holonomicDriveController.calculate(
-        robotPose, 
-        desiredTrajectory.sample(driveTimer.get()), // m/s
-        closestScoringPose.getRotation()
-      );
-
-      driveTrain.setDesiredSpeedsFromHolonomicController(speeds);
-      doesCalculateScoringPosition = false;
-    }
+    driveTrain.setDesiredSpeedsFromHolonomicController(speeds);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    System.out.println("done driving to pose");
+    driveTimer.stop();
+    driveTimer.reset();
   }
 
   // Returns true when the command should end.
